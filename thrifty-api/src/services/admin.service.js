67159941df4e7705003318f6819e51a5b940fa79
Kingsley <kifeanyi216@gmail.com>
2026-05-11 18:58:35 +0100

    fix: remove debug log from admin login

--- thrifty-api/src/services/admin.service.js
+++ thrifty-api/src/services/admin.service.js
@@ -643,9 +643,6 @@ export const adminService = {
       user_agent: userAgent,
     });
 
-    // ADD THIS TEMPORARILY
-    logger.info({ tokenFingerprint, adminId: admin.id }, "Session inserted");
-
     // sign JWT
     const jwtToken = jwt.sign(
       {
