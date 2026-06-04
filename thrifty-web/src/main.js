f928a98d11833b7456e3ebd969811b0958156d96
Kingsley <kifeanyi216@gmail.com>
2026-06-04 21:37:16 +0100

    fix: remove mounting comment

--- thrifty-web/src/main.js
+++ thrifty-web/src/main.js
@@ -6,7 +6,6 @@ import App from "./App.vue";
 import router from "./router";
 
 // create the Vue application instance
-// this is the main entry point of the application where we set up the Vue app, integrate Pinia for state management, and Vue Router for routing, and then mount the app to the DOM.
 const app = createApp(App);
 
 // use Pinia and Vue Router in the application
